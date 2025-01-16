3a8c5f9ea98de58f4b31a2f676f9b8ec309c04fd
acctzsy198 <acctzsy198@sina.com>
2025-01-16 23:12:44 +0800

    Update port configuration for Railway deployment

--- web_server.py
+++ web_server.py
@@ -750,4 +750,5 @@ def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 8080))
+    socketio.run(app, debug=False, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
